6f99056579bc75a62c1a450f0e219e7255ff7f0e
Richard Yee <richtomyee@gmail.com>
2016-11-18 06:37:50 -0800

    d

--- js/station-click.js
+++ js/station-click.js
@@ -1,7 +1,8 @@
 $(window).load(function(){
     $.gsap.enabled(true);
     var tracker=$("#svgObject");
-    var node=$(".station");
+    var svgDoc = tracker.contentDocument;
+    var node=svgDoc.getElementsByClass("station");
     var animationtime;
 
     node.click(function(){
@@ -10,11 +11,13 @@ $(window).load(function(){
       if (tracker.hasClass("destination-selected") ){
         tracker.removeClass("destination-selected");
         //keep replaced start data; this essentially overwrites old start data
+        node.setAttribute("fill", "blue");
         $(e.target).style.fill="blue";
   		}
   		else {
         tracker.addClass("destination-selected");
         //send data; might overwrite old destination data
+        node.setAttribute("fill", "green");
         $(e.target).style.fill="green";
   		}
 		}
@@ -22,11 +25,13 @@ $(window).load(function(){
       if (tracker.hasClass("destination-selected") ){
         tracker.addClass("start-selected");
         //send data; overwrite scenario highly likely
+        node.setAttribute("fill", "red");
         $(e.target).style.fill="red";
   		}
   		else {
         tracker.addClass("start-selected");
         //keep start data; expected entry point
+        node.setAttribute("fill", "yellow");
         $(e.target).style.fill="yellow";
   		}
 		}
